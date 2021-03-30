dbb9a580aa8f5d1b404ee9d3198082e4d342a53b
Frank Laercio <franklaercio@gmail.com>
2021-03-30 00:18:00 -0300

    Adding api in pages

---
+++ web/src/pages/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: 'Frank' },
+    { id: 1, name: 'Ohanna' },
+    { id: 1, name: 'Olívia' }
+  ];
+
+  return response.json(users);
+}
