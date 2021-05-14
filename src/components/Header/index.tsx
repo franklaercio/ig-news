ad80d60d01641d1a70b1807213817602e39718db
franklaercio <franklaercio@gmail.com>
2021-05-13 23:49:14 -0300

    Adding Link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 
 import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
+import Link from 'next/link';
 
 export function Header() {
   return (
@@ -8,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
